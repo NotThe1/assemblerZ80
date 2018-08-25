3d0a695de4fa650397e0c6591aecc0c73e78edc3
Frank Martyn <FrankMartyn1945@gmail.com>
2018-08-25 10:57:40 -0400

    Fixed IN A,(C) problem

--- assemblerZ80/src/assembler/InstructionSet.java
+++ assemblerZ80/src/assembler/InstructionSet.java
@@ -375,8 +375,10 @@ public class InstructionSet {
 	private static OpCodeNode rootIN() {
 		root = new OpCodeNode(Z80.patWord, Z80.BAD_OPCODE);
 		
-//		branch = new OpCodeNode[] { new OpCodeNode(Z80.patLIT_A, Z80.BAD_OPCODE),
-//				 new OpCodeNode(Z80.patIND_C, "IN_1"),new OpCodeNode(Z80.patEXP_ADD, "IN_2") };
+		branch = new OpCodeNode[] { new OpCodeNode(Z80.patLIT_A, Z80.BAD_OPCODE),
+				 new OpCodeNode(Z80.patIND_C, "IN_1") };
+		root.addBranch(branch);
+
 
 		branch = new OpCodeNode[] { new OpCodeNode(Z80.patLIT_A, Z80.BAD_OPCODE),
 		new OpCodeNode(Z80.patEXP_ADD, "IN_2") };
